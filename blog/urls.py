0ee20f6ad983597cd4c2d853a9ee55da8d1f5794
alias-3 <wanianurag3@gmail.com>
2018-08-01 02:24:15 +0530

    edit, create blogs from site itself,admin authentication required

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
 	path("", views.home, name='home'),
-	path("blog_list",views.blog_list,name= 'blog_list')
+	path("blog_list/",views.blog_list,name= 'blog_list'),
+	path("blog_list/<int:pk>",views.blogpost, name= 'blogpost'),
+	path("blog_list/newpost/",views.newpost, name="newpost"),
+	path("blog_list/<int:pk>/edit/",views.editpost, name= 'editpost')
 ] 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 def home(request):
@@ -8,3 +9,39 @@ def home(request):
 def blog_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/blog_list.html', {'posts': posts})
+
+def blogpost(request,pk):
+	post = get_object_or_404(Post,pk=pk)
+	return render(request, 'blog/blogpost.html', {'post': post})
+
+def newpost(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('blogpost', pk = post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/editpost.html', {'form': form} )
+
+
+def editpost(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance= post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('blogpost', pk = post.pk)
+	else:
+		form = PostForm(instance = post)
+	return render(request, 'blog/editpost.html', {'form': form} )
+
+
+
+
